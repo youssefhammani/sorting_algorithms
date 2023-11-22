2ba66713ab1ec9e299775f031a5dae7495bbc0b2
youssefhammani <youssefhammani04@gmail.com>
2023-11-22 06:59:31 -0800

    Insertion sort

--- sort.h
+++ sort.h
@@ -33,6 +33,7 @@ void print_list(const listint_t *list);
 /*======================================================================*/
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 
 #endif /* __SORT__ */
