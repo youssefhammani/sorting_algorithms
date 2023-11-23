4895f516f85b93210686ceae576b0583d8f26775
youssefhammani <youssefhammani04@gmail.com>
2023-11-23 15:45:41 -0800

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -41,7 +41,7 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 		}
 	}
 
-	if (array[i + 1] > array[high])
+	if (array[i + 1] > array[low])
 	{
 		swap(&array[i + 1], &array[high]);
 		print_array(array, size);
